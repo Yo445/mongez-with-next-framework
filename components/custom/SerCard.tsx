044be43711607088f1e3b690f72d32fcfd728302
Yo445 <ya108942@gmail.com>
2025-01-02 16:08:31 +0200

    add card

--- components/custom/SerCard.tsx
+++ components/custom/SerCard.tsx
@@ -62,3 +62,35 @@ const SerCard: React.FC<SerCardProps> = ({ initialTitle, link, icon }) => {
 };
 
 export default SerCard;
+
+
+
+
+
+{/* <div onClick={handleCardClick}
+className="relative flex min-h-screen flex-col justify-center overflow-hidden bg-gray-50 py-6 sm:py-12">
+    <div
+        className="group relative cursor-pointer overflow-hidden bg-white px-6 pt-10 pb-8 shadow-xl ring-1 ring-gray-900/5 transition-all duration-300 hover:-translate-y-1 hover:shadow-2xl sm:mx-auto sm:max-w-sm sm:rounded-lg sm:px-10">
+        <span className="absolute top-10 z-0 h-20 w-20 rounded-full bg-[#f9f1cf] transition-all duration-300 group-hover:scale-[10]"></span>
+        <div className="relative z-10 mx-auto max-w-md">
+            <span className="grid h-20 w-20 place-items-center rounded-full bg-[#f9f1cf] transition-all duration-300 group-hover:bg-[#f9f1cf]">
+                <div className="h-10 w-10 transition-all">
+                {IconElement}
+                </div>
+            </span>
+            <div
+                className="space-y-6 pt-5 text-base leading-7 text-gray-600 transition-all duration-300 group-hover:text-white/90">
+                  {initialTitle}
+            </div>
+            <div className="pt-5 text-base font-semibold leading-7">
+                <p>
+                    <div  className="text-gray-500 transition-all duration-300 group-hover:text-white">
+                      Emirates
+                    </div>
+                </p>
+            </div>
+        </div>
+    </div>
+</div> */}
+
+
